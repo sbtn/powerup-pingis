7c16b191b3477bc1370576f95957e8210680c092
sbtn <sbtn@users.noreply.github.com>
2018-11-20 19:51:14 +0100

    eslint

--- server/server.js
+++ server/server.js
@@ -6,31 +6,30 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log("New user connected.");
+  console.log('New user connected.');
 });
 
 app.get('/score/:player', (req, res) => {
-    let player = req.params.player;
-    
-    io.emit('score', { player });
-    res.status(200).end();
-})
+  const { player } = req.params;
+  io.emit('score', { player });
+  res.status(200).end();
+});
 
 app.get('/reset-streak', (req, res) => {
-    io.emit('resetStreak');
-    res.status(200).end();
+  io.emit('resetStreak');
+  res.status(200).end();
 });
 
 app.get('/restart', (req, res) => {
-    io.emit('restart');
-    res.status(200).end();
+  io.emit('restart');
+  res.status(200).end();
 });
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
